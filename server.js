b3cbc4a2f0bb916de277a9a0daacc39701d80d6e
Omer Obaid <141782449+O-OBAID@users.noreply.github.com>
2024-10-25 19:17:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { connectDb } = require("./Config/dbConnection");
 const userRoute = require("./Routes/UserRoute/user.route");
 const movieRoute = require("./Routes/MovieRoute/movie.route");
-const runMigration = require("./migration");
+// const runMigration = require("./migration");
 
 app.use(express.json());
 dotenv.config();
@@ -24,5 +24,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
   connectDb();
-  runMigration();
+  // runMigration();
 });
